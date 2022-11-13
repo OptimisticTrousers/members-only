9b39a49be9f773369460ce7e0472a1bcb6a01699
OptimisticTrousers <tony.isern@yahoo.com>
2022-11-12 21:26:20 -0500

    Setting up bcrypt and validating form fields

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
 var mongoose = require("mongoose");
 require("dotenv").config();
+var bcrypt = require("bcryptjs");
 
 var indexRouter = require("./routes/index");
 var postRouter = require("./routes/post");
@@ -36,17 +37,22 @@ app.use(
 
 passport.use(
   new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
+    User.findOne({ username }, (err, user) => {
       if (err) {
         return done(err);
       }
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (res) {
+          // passwords! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
     });
   })
 );
@@ -61,13 +67,6 @@ passport.deserializeUser(function (id, done) {
   });
 });
 
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(logger("dev"));
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,7 @@
+const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
 exports.index = function (req, res, next) {
   res.render("index", { title: "Clubhouse" });
 };
@@ -6,14 +10,51 @@ exports.signup_get = function (req, res, next) {
   res.render("signup_form");
 };
 
+// Handle sign up on POST
+exports.signup_post = [
+  // Validate and sanitize fields.
+  body("firstName").trim().isLength({ min: 1 }).escape(),
+  body("lastName").trim().isLength({ min: 1 }).escape(),
+  body("email").isEmail().normalizeEmail(),
+  body("password").trim().isLength({ min: 1 }).escape(),
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract validation errors from a request.
+    const errors = validationResult(req);
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashedPassword,
+      })
+
+      if(!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values/error messages.
+        
+      } else {
+        user.save(function (err) {
+          if(err) {
+            return next(err)
+          }
+
+          // Successful - redirect to home page
+          res.redirect("/")
+        })
+      }
+    });
+  },
+];
+
 exports.login_get = function (req, res, next) {
   res.render("login_form");
 };
 
 exports.membership_get = function (req, res, next) {
-  res.render("vip_form")
-}
+  res.render("vip_form");
+};
 
 exports.admin_get = function (req, res, next) {
-  res.render("vip_form")
-}
+  res.render("vip_form");
+};
